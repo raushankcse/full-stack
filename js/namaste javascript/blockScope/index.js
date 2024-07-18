493c8f26bbc441bf61c12077bdbd2a231614df3f
Raushan Kumar <raushankcse@gmail.com>
2024-07-18 19:20:24 +0530

    scope git pus

---
+++ js/namaste javascript/blockScope/index.js
@@ -0,0 +1,35 @@
+// // {
+// //   // compound statement
+// //   var a = 10;
+// //   console.log(a);
+// // }
+
+// // if(true) {
+
+// // }
+
+// var a = 100;
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// console.log(a);
+// //   console.log(b);
+// //   console.log(c);
+
+
+const a = 20;
+{
+  const a = 100;
+  {
+    const a = 200;
+    // console.log(a);
+  }
+}
+
+console.log(a);
