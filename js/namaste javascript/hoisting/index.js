26d839e5db7c92fd6cba963c0abc4c86efd58d0a
raushan kumar <raushankcse@gmail.com>
2024-07-18 17:52:09 +0530

    namaste

---
+++ js/namaste javascript/hoisting/index.js
@@ -0,0 +1,12 @@
+
+
+var x = 4;
+
+function  getName(){
+    console.log("namaste Javascript");
+}
+
+
+getName();
+console.log(x);
+console.log(getName);
---
+++ js/namaste javascript/undefined/index.js
@@ -0,0 +1,25 @@
+
+// var a;
+
+// console.log(a);
+
+
+// if(a===undefined){
+//     console.log("a is undefinded");
+// }else{
+//     console.log("a is not undefined");
+// }
+
+var a;
+console.log(a);
+a=10;
+console.log(a);
+a = "hello world";
+console.log(a);
+
+
+
+// not recommended
+// a = undefined;
+// console.log(a);
+
